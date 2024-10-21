ede6d2c31762d7f5625418afdf4c9e1b4478f2ef
Bazch <mikael.vidal@gmail.com>
2024-10-21 08:50:32 -0300

    feat: base user model

---
+++ core/models/base_model.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+from django.utils import timezone
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+  
+    class Meta:
+        abstract = True
