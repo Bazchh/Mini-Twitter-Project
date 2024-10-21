0e71c661b10ade7c9f47f24103145cbe6dc1d6d0
Bazch <mikael.vidal@gmail.com>
2024-10-21 17:33:33 -0300

    refact: retrieve method

--- core/services/profile_service.py
+++ core/services/profile_service.py
@@ -3,6 +3,8 @@ from core.shared.customAPIException import CustomAPIException
 from django.core.exceptions import ObjectDoesNotExist
 from core.models.profile_model import UserProfile
 from rest_framework.exceptions import PermissionDenied
+from core.repositories.user_repository import UserRepository
+
 class UserProfileService:
     @staticmethod
     def list_all_profiles():
@@ -20,16 +22,6 @@ class UserProfileService:
             return profile
         except Exception as e:
             raise CustomAPIException(detail=str(e), status_code=500)
-        
-    @staticmethod
-    def retrieve_profile(profile_id):
-        try:
-            profile = UserProfileRepository.get_profile_by_id(profile_id)
-            if not profile:
-                raise CustomAPIException(detail='Profile not found.', status_code=404)
-            return profile
-        except Exception as e:
-            raise CustomAPIException(detail=str(e), status_code=500)
 
 
     @staticmethod
@@ -42,14 +34,11 @@ class UserProfileService:
     @staticmethod
     def update_profile(logged_in_user, validated_data):
         try:
-            # Busca o perfil associado ao usuário logado
             profile = UserProfile.objects.get(user_id=logged_in_user.id)
             
-            # Verifica se o usuário logado é o dono do perfil
             if profile.user_id != logged_in_user.id:
                 raise CustomAPIException(detail="You do not have permission to update this profile.", status_code=403)
             
-            # Atualiza os atributos do perfil
             for attr, value in validated_data.items():
                 setattr(profile, attr, value)
             
@@ -70,10 +59,10 @@ class UserProfileService:
             if not profile:
                 raise CustomAPIException(detail="Profile not found.", status_code=404)
             
-            if profile.user != current_user:
+            if profile.user.id != current_user.id:
                 raise PermissionDenied("You can only delete your own profile.")
-
-            UserProfileRepository.delete_profile(user_id)
+            
+            UserRepository.delete_user(user_id) 
         except PermissionDenied as e:
             raise CustomAPIException(detail=str(e), status_code=403)
         except Exception as e:
