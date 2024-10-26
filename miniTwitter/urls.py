7f882a1a37fedfd9da660fd8af4db9c6da904bfb
Bazch <mikael.vidal@gmail.com>
2024-10-26 17:47:58 -0300

    chore: files update

--- miniTwitter/urls.py
+++ miniTwitter/urls.py
@@ -8,6 +8,8 @@ from core.views.token_viewset import CustomTokenPairView
 from core.views.user_viewset import UserViewSet
 from core.views.profile_viewset import UserProfileViewSet
 from core.views.post_viewset import PostViewSet
+from core.views.user_following_viewset import UserFollowingViewSet
+from core.views.follow_request_viewset import FollowRequestViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -22,8 +24,18 @@ urlpatterns = [
     path('api/token/', CustomTokenPairView.as_view(), name='token_obtain_pair'),
         
     path('likes/<uuid:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
-    path('likes/<uuid:pk>/likes/', PostViewSet.as_view({'get': 'get_likes'}), name='post-get-likes')
-]
+    path('likes/<uuid:pk>/likes/', PostViewSet.as_view({'get': 'get_likes'}), name='post-get-likes'),
+    
+    path('follow_requests/user_requests', FollowRequestViewSet.as_view({'get': 'list'}), name='user_requests'),
+    path('follow_requests/send_request/<uuid:user_id>/', FollowRequestViewSet.as_view({'post': 'create'})),
+    path('follow_requests/reject-accept/<uuid:pk>/handle/<str:action>/', FollowRequestViewSet.as_view({'put': 'handle_request'})),
+    
+    path('followings/following/', UserFollowingViewSet.as_view({'get': 'list_following'}), name='list-following'),
+    path('followings/followers/', UserFollowingViewSet.as_view({'get': 'list_followers'}), name='list-followers'),
+    path('followings/remove-follower/<uuid:follower_id>/', UserFollowingViewSet.as_view({'delete': 'remove_follower'}), name='remove-follower'),
+    path('followings/unfollow/<uuid:followed_id>/', UserFollowingViewSet.as_view({'delete': 'unfollow_user'}), name='unfollow-user'),
+    path('followings/non_friends/', UserFollowingViewSet.as_view({'get': 'list_non_friends'}), name='list-non-friends')
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
